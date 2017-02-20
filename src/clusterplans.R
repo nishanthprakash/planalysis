554f1a4705a717da787f8196e93958ceb9e96c93
Nishanth Prakash <nishanthprakash20@gmail.com>
2017-02-20 14:35:06 -0500

    plan clusters on disj interval stats

--- src/clusterplans.R
+++ src/clusterplans.R
@@ -1,3 +1,5 @@
+library(moments)
+
 setwd("~/Projects/Plan Composition/pyret-starter/planalysis/data")
 
 mfs = list.files(path = "./measures/")
@@ -7,9 +9,10 @@ mfs2 = unique(lapply(mfs1, function (i) strsplit(i[2], "_")[[1]]))
 msubs <<- unique(lapply(mfs2, function (i) i[1]))
 mtcs <<- unique(lapply(mfs2, function (i) strsplit(i[2], "[.]")[[1]][1]))
 
+unlink(file.path('.', 'plots/planclusters'), recursive = TRUE, force = FALSE)
+dir.create(file.path('.', 'plots/planclusters'), showWarnings = FALSE)
+
 for (mfn in mfns){
-  if (mfn > 1) 
-    break
   for (tc in mtcs){
     disj = list()
     maxints = 0
@@ -31,7 +34,6 @@ for (mfn in mfns){
       
       max = sfs[[2]][which.max(abs(as.numeric(sfs[[2]])))]
       len = length(disj[[sub]])
-      #maxints = if (maxints < len) len else maxints
     }
     m = NULL
     maxints = 0
@@ -43,8 +45,67 @@ for (mfn in mfns){
       disjel = disjel/tail(disjel, n=1)
       m = rbind(m, c(disjel, rep(1, 2*maxints - length(disjel))))
     }
-    #png(paste("./plots/planclusters/", mfn, "-", tc,".png", sep=""), width=12,height=6,units="in", res=800)
+    png(paste("./plots/planclusters/int_", mfn, "-", tc,".png", sep=""), width=12,height=6,units="in", res=800)
+    row.names(m) = names(disj)
+    plot.new()
     plot(hclust(dist(m)))
-    #dev.off()
+    dev.off()
+    
+    n1 = NULL
+    n2 = NULL
+    n3 = NULL
+    n4 = NULL
+    for(i in 1:length(disj)){
+      maxpos = tail(disj[[i]], n=1)[[2]] - 1
+      disj[[i]] = (disj[[i]]-1)/maxpos
+      
+      countint = length(disj[[i]][[1]])
+      startend = disj[[i]][[2]] - disj[[i]][[1]]
+      
+      startmean = mean(disj[[i]][[1]])
+      endmean = mean(disj[[i]][[2]])
+      lengthmean = mean(startend)
+      
+      startdev = sd(disj[[i]][[1]])
+      enddev = sd(disj[[i]][[2]])
+      lengthdev = sd(startend)
+      
+      startsk = skewness(disj[[i]][[1]])
+      endsk = skewness(disj[[i]][[2]])
+      lengthsk = skewness(startend)
+      
+      startku = kurtosis(disj[[i]][[1]])
+      endku = kurtosis(disj[[i]][[2]])
+      lengthku = kurtosis(startend)
+      
+      n1 = rbind(n1, c(startmean, endmean, lengthmean, startdev, enddev, lengthdev, startsk, endsk, lengthsk, startku, endku, lengthku))
+      n2 = rbind(n2, c(startmean, endmean, lengthmean, startdev, enddev, lengthdev, startsk, endsk, lengthsk))
+      n3 = rbind(n3, c(startmean, endmean, lengthmean, startdev, enddev, lengthdev))
+      n4 = rbind(n4, countint)
+    }
+      
+    png(paste("./plots/planclusters/stat1_", mfn, "-", tc,".png", sep=""), width=12,height=6,units="in", res=800)
+    row.names(n1) = names(disj)
+    plot.new()
+    plot(hclust(dist(n1)))
+    dev.off()
+
+    png(paste("./plots/planclusters/stat2_", mfn, "-", tc,".png", sep=""), width=12,height=6,units="in", res=800)
+    row.names(n2) = names(disj)
+    plot.new()
+    plot(hclust(dist(n2)))
+    dev.off()
+    
+    png(paste("./plots/planclusters/stat3_", mfn, "-", tc,".png", sep=""), width=12,height=6,units="in", res=800)
+    row.names(n3) = names(disj)
+    plot.new()
+    plot(hclust(dist(n3)))
+    dev.off()
+    
+    png(paste("./plots/planclusters/stat4_", mfn, "-", tc,".png", sep=""), width=12,height=6,units="in", res=800)
+    row.names(n4) = names(disj)
+    plot.new()
+    plot(hclust(dist(n4)))
+    dev.off()
   }
 }
